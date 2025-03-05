294e179fe1da98b0904a7632f535ed60ac6d988e
KevinFD95 <emkf_95@hotmail.com>
2025-03-05 15:51:25 +0100

    2025/03/04 - Iconos y pantalla de carga

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+    useEffect(() => {
+      setTimeout(async () => {
+        await SplashScreen.hideAsync(); // Oculta el Splash después del tiempo deseado
+      }, 2000); // <-- Cambia el número de milisegundos según lo que necesites
+    }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
