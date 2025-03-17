dce3c029cdbd6556165adf95bfc33f42a109b2d7
KevinFD95 <emkf_95@hotmail.com>
2025-03-17 12:10:22 +0100

    2025/03/17 - Instalacion lint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,23 +1,22 @@
-// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  
+
   extends: [
     "expo",
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
     "prettier/prettier": ["error"],
     "react/react-in-jsx-scope": "off",
