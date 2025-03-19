4b008cf460bab509d5bcf7498865eb915143d5a9
Fran Ojeda <fojeama@gmail.com>
2025-03-19 16:56:02 +0100

    20250319 - TextInputs
    Añadido el componente de los TextInputs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useEffect } from "react";
+import { useState } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import CustomButton from "./components/button";
 import * as SplashScreen from "expo-splash-screen";
 import color from "./config/colors.json";
+import { CustomTextBox, CustomTextBoxFind } from "./components/text-input";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -15,6 +17,8 @@ export default function App() {
       await SplashScreen.hideAsync(); // Oculta el Splash después del tiempo deseado
     }, 2000); // Cambia el número de milisegundos según lo que necesites
   }, []);
+  const [textInput, setTextInput] = useState("");
+  const [textFind, setTextFind] = useState("");
 
   return (
     <SafeAreaProvider>
@@ -24,6 +28,16 @@ export default function App() {
           <View style={styles.view}>
             <Text>Bibliapp</Text>
             <CustomButton text="Iniciar" onPress={() => alert("Botón")} />
+            <CustomTextBox
+              value={textInput}
+              onChangeText={setTextInput}
+              placeholder=" "
+            />
+            <CustomTextBoxFind
+              value={textFind}
+              onChangeText={setTextFind}
+              placeholder="Búsqueda"
+            />
           </View>
         </SafeAreaView>
       </NavigationContainer>
---
+++ frontend/src/components/text-input.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { TextInput, StyleSheet } from "react-native";
+import color from "../config/colors.json";
+
+export function CustomTextBox({
+  value = "",
+  onChangeText = () => {},
+  placeholder = "",
+}) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
+  return (
+    <TextInput
+      style={[styles.input, isFocused && styles.inputFocused]}
+      value={value}
+      onChangeText={onChangeText}
+      placeholder={placeholder}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    width: "100%",
+    marginVertical: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    color: color["black"],
+    fontSize: 16,
+    borderWidth: 1,
+    backgroundColor: "white",
+    borderColor: color.icons["unselected-icons"],
+  },
+  inputFocused: {
+    borderWidth: 3,
+    borderColor: color["button-background"],
+  },
+});
+
+export function CustomTextBoxFind({
+  value = "",
+  onChangeText = () => {},
+  placeholder = "",
+}) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
+  return (
+    <TextInput
+      style={[stylesf.input, isFocused && stylesf.inputFocused]}
+      value={value}
+      onChangeText={onChangeText}
+      placeholder={placeholder}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    />
+  );
+}
+
+const stylesf = StyleSheet.create({
+  input: {
+    width: "100%",
+    marginVertical: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 20,
+    color: color["black"],
+    fontSize: 16,
+    borderWidth: 1,
+    backgroundColor: "white",
+    borderColor: color.icons["unselected-icons"],
+  },
+  inputFocused: {
+    borderWidth: 3,
+    borderColor: color["button-background"],
+  },
+});
