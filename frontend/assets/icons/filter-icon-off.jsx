931e4558c6f594d9ab2f0fcafe70a9dcaeb88fdd
Kevin <emkf_95@hotmail.com>
2025-03-21 17:55:22 +0100

    2025/03/21 - Nuevos iconos

---
+++ frontend/assets/icons/filter-icon-off.jsx
@@ -0,0 +1,11 @@
+import Svg, { Path } from "react-native-svg";
+
+const FilterIcon = ({ size, color }) => {
+  return (
+    <Svg height={size} width={size} viewBox="0 -960 960 960" fill={color}>
+      <Path d="M440-160q-17 0-28.5-11.5T400-200v-240L168-736q-15-20-4.5-42t36.5-22h560q26 0 36.5 22t-4.5 42L560-440v240q0 17-11.5 28.5T520-160h-80Zm40-308 198-252H282l198 252Zm0 0Z" />
+    </Svg>
+  );
+};
+
+export default FilterIcon;
