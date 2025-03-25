6a645c116c4098895b80bc6f0b9f68f17b9f2e76
Joan005 <108304520+Joan005@users.noreply.github.com>
2025-03-25 14:03:53 +0100

    25/03/25-errores2

--- frontend/src/app/profile.jsx
+++ frontend/src/app/profile.jsx
@@ -1,37 +1,76 @@
-import { ScrollView, Text, StyleSheet } from "react-native";
-
+import { ScrollView, Text, StyleSheet, View, Image } from "react-native";
+import viewStyle from "../styles/view-styles.jsx";
 export default function HomeStackNavigator() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.titleStyle}>Perfil</Text>
+
+    <ScrollView contentContainerStyle={viewStyle.mainContainer}>
+      <View style={styles.box}>
+      <Image
+          source={{
+            uri: "https://e7.pngegg.com/pngimages/410/195/png-clipart-pencil-drawing-pencil-angle-pencil.png"
+          }}
+          style={{height:50,width:50}} 
+        />
+        <View style={{alignItems: "center"}}>
+                <Image
+                  source={{
+                    uri: "https://media.istockphoto.com/id/1495088043/es/vector/icono-de-perfil-de-usuario-avatar-o-icono-de-persona-foto-de-perfil-símbolo-de-retrato.jpg?s=612x612&w=0&k=20&c=mY3gnj2lU7khgLhV6dQBNqomEGj3ayWH-xtpYuCXrzk=",
+                  }}
+                  style={styles.image} 
+                />
+                      <Text style={{marginTop:10,marginBottom:50}}>Icono Perfil</Text>
+              </View>
+
+        <Image
+          source={{
+            uri: "https://e7.pngegg.com/pngimages/529/757/png-clipart-computer-icons-nut-bolt-desktop-wallpaper-cog.png"
+          }}
+          style={{height:50,width:50}} 
+        />
+      </View>
+
+      <View style={{marginBottom:20}}>
+        <Text>Nombre de Usuario: </Text>
+        <Text>joan005</Text>
+      </View> 
+      <View style={{marginBottom:20}}>
+        <Text>Correo Electronico: </Text>
+        <Text>joancarmona05@gmail.com</Text>
+      </View>
+      <View style={{marginBottom:20}}>
+        <Text>Apellido: </Text>
+        <Text>Carmona</Text>
+      </View>
+      <View style={{marginBottom:20}}>
+        <Text>Direccion: </Text>
+        <Text>C/ MiCasa Nº1</Text>
+      </View>
+      <View style={{marginBottom:20}}>
+        <Text>Nombre: </Text>
+        <Text>joan</Text>
+      </View>     
+
+      <View style={{alignItems: "center"}}>
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/7175/7175236.png"
+          }}
+          style={{height:50,width:50,marginTop:"25%"}} 
+        />
+      </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 15,
-    paddingRight: 30,
-    paddingBottom: 15,
-    paddingLeft: 30,
+  box: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-
-  titleStyle: {
-    marginBottom: 15,
-    fontSize: 20,
-    fontWeight: "bold",
-    fontFamily: "Helvetica",
-  },
-
-  list: {
-    marginBottom: 30,
-    gap: 20,
-  },
-
-  books: {
-    flex: 1,
-    width: 100,
+  image: {
     height: 150,
-    resizeMode: "contain",
+    width: 150,
+    alignItems: "center",
   },
 });
