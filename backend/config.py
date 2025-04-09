e94e491b54c97eed75f5f152a751cedfaedcb103
Kevin <emkf_95@hotmail.com>
2025-04-09 23:27:24 +0200

    Variables de entorno para Host, port y debug

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,10 @@ load_dotenv()
 
 
 class Config:
+    FLASK_HOST = os.getenv("FLASK_HOST")
+    FLASK_PORT = os.getenv("FLASK_PORT")
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG")
+
     MYSQL_HOST = os.getenv("DB_HOST")
     MYSQL_USER = os.getenv("DB_USER")
     MYSQL_PASSWORD = os.getenv("DB_PASSWORD")
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 # main.py
 from app import create_app
+from config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=Config.FLASK_HOST, port=Config.FLASK_PORT, debug=Config.FLASK_DEBUG)
