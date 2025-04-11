610e7a4943e1a8b18d68a2e0870837e38cc61369
KevinFD95 <emkf_95@hotmail.com>
2025-04-11 18:08:54 +0200

    Formateo de archivos

--- frontend/src/app/config.jsx
+++ frontend/src/app/config.jsx
@@ -1,61 +1,54 @@
 import { useState } from "react";
 import { ScrollView, Text, StyleSheet, View, Image } from "react-native";
 import viewStyle from "../styles/view-styles.jsx";
-import SwitchComponent from "../components/switch.jsx"; 
+import SwitchComponent from "../components/switch.jsx";
 
 export default function HomeStackNavigator() {
-  const [switchState, setSwitchState] = useState(false);  
+  const [switchState, setSwitchState] = useState(false);
 
-  const handleSwitchChange = (newState) => {
-    setSwitchState(newState);
-    console.log("Nuevo estado del switch:", newState);  
+  const handleSwitchChange = () => {
+    setSwitchState(switchState);
   };
 
   return (
     <ScrollView contentContainerStyle={viewStyle.mainContainer}>
       <View style={styles.row}>
         <Text>Tema</Text>
-        <Image 
-          source={{ uri: "https://e7.pngegg.com/pngimages/714/986/png-clipart-art-graphy-icon-sun-photography-sunlight.png" }} 
-          style={[styles.image, { marginLeft: 200 }]}  // Añadimos un margen derecho
+        <Image
+          source={{
+            uri: "https://e7.pngegg.com/pngimages/714/986/png-clipart-art-graphy-icon-sun-photography-sunlight.png",
+          }}
+          style={[styles.image, { marginLeft: 200 }]} // Añadimos un margen derecho
         />
-        <Image 
-          source={{ uri: "https://e7.pngegg.com/pngimages/1018/540/png-clipart-computer-icons-symbol-star-and-crescent-symbol-miscellaneous-leaf.png" }} 
-          style={[styles.image, { marginLeft: 5 }]}  // Estilo con margen
+        <Image
+          source={{
+            uri: "https://e7.pngegg.com/pngimages/1018/540/png-clipart-computer-icons-symbol-star-and-crescent-symbol-miscellaneous-leaf.png",
+          }}
+          style={[styles.image, { marginLeft: 5 }]} // Estilo con margen
         />
       </View>
 
       <View style={styles.row}>
         <Text style={styles.text}>Notificaciones</Text>
-        <SwitchComponent  
-          onChange={handleSwitchChange} 
-        />
+        <SwitchComponent onChange={handleSwitchChange} />
       </View>
       <View style={styles.row}>
         <Text style={styles.text}>Recordatorio de lectura</Text>
-        <SwitchComponent  
-          onChange={handleSwitchChange} 
-        />
+        <SwitchComponent onChange={handleSwitchChange} />
       </View>
       <View style={styles.row}>
         <Text style={styles.text}>Recomendaciones de libros</Text>
-        <SwitchComponent  
-          onChange={handleSwitchChange} 
-        />
+        <SwitchComponent onChange={handleSwitchChange} />
       </View>
       <View style={styles.row}>
         <Text style={styles.text}>Modificar metodo de pago</Text>
-        
       </View>
       <View style={styles.row}>
         <Text style={styles.text}>Suscripcion premium</Text>
-      
       </View>
       <View style={styles.row}>
         <Text style={styles.text}>Historial de pagos</Text>
-      
       </View>
-      
     </ScrollView>
   );
 }
@@ -69,14 +62,14 @@ const styles = StyleSheet.create({
   image: {
     height: 50,
     width: 50,
-    marginRight: 15,  // Esto agrega un margen entre las imágenes
+    marginRight: 15, // Esto agrega un margen entre las imágenes
   },
   text: {
     marginBottom: 20,
   },
   row: {
-    flexDirection: 'row',  // Esto hace que los elementos estén alineados en una fila
-    alignItems: 'center',  // Alinea verticalmente los elementos
-    marginBottom: 20,  // Opcional: puedes agregar un poco de espacio entre filas
-  }
+    flexDirection: "row", // Esto hace que los elementos estén alineados en una fila
+    alignItems: "center", // Alinea verticalmente los elementos
+    marginBottom: 20, // Opcional: puedes agregar un poco de espacio entre filas
+  },
 });
--- frontend/src/app/profile-edit.jsx
+++ frontend/src/app/profile-edit.jsx
@@ -1,25 +1,23 @@
-import { ScrollView, Text, StyleSheet, View, Image, TextInput } from "react-native";
+import { ScrollView, Text, StyleSheet, View, Image } from "react-native";
 import viewStyle from "../styles/view-styles.jsx";
 import { CustomTextBox } from "../components/text-input.jsx";
 export default function HomeStackNavigator() {
   return (
-
     <ScrollView contentContainerStyle={viewStyle.mainContainer}>
+      <View style={{ alignItems: "center" }}>
+        <Image
+          source={{
+            uri: "https://media.istockphoto.com/id/1495088043/es/vector/icono-de-perfil-de-usuario-avatar-o-icono-de-persona-foto-de-perfil-símbolo-de-retrato.jpg?s=612x612&w=0&k=20&c=mY3gnj2lU7khgLhV6dQBNqomEGj3ayWH-xtpYuCXrzk=",
+          }}
+          style={styles.image}
+        />
+        <Text style={{ marginTop: 10, marginBottom: 50 }}>Icono Perfil</Text>
+      </View>
 
-        <View style={{alignItems: "center"}}>
-                <Image
-                  source={{
-                    uri: "https://media.istockphoto.com/id/1495088043/es/vector/icono-de-perfil-de-usuario-avatar-o-icono-de-persona-foto-de-perfil-símbolo-de-retrato.jpg?s=612x612&w=0&k=20&c=mY3gnj2lU7khgLhV6dQBNqomEGj3ayWH-xtpYuCXrzk=",
-                  }}
-                  style={styles.image} 
-                />
-                      <Text style={{marginTop:10,marginBottom:50}}>Icono Perfil</Text>
-              </View>
-      
       <View style={styles.text}>
         <Text>Nombre de Usuario: </Text>
         <CustomTextBox value={"joan005"} />
-      </View> 
+      </View>
       <View style={styles.text}>
         <Text>Correo Electronico: </Text>
         <CustomTextBox value={"joancarmona05@gmail.com"} />
@@ -52,6 +50,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   text: {
-    marginBottom:20
-  }
+    marginBottom: 20,
+  },
 });
